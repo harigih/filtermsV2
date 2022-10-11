b7e484acbeb9c10cb3919aec5763729f5b26e8b5
MS <108895850+Saifudeebot@users.noreply.github.com>
2022-10-11 15:34:05 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -45,7 +45,7 @@ async def start(client, message):
             InlineKeyboardButton("➕️ 𝙰𝙳𝙳 𝙼𝙴 𝚃𝙾 𝚈𝙾𝚄𝚁 𝙶𝚁𝙾𝚄𝙿 ➕️", url=f"http://t.me/{temp.U_NAME}?startgroup=true")
             ],[
             InlineKeyboardButton("🔍 𝚂𝙴𝙰𝚁𝙲𝙷 🔍", switch_inline_query_current_chat=''), 
-            InlineKeyboardButton("💠 𝙲𝙷𝙰𝙽𝙽𝙴𝙻 💠", url="https://t.me/tamilhb")
+            InlineKeyboardButton("💠 𝙲𝙷𝙰𝙽𝙽𝙴𝙻 💠", url="https://t.me/+AaJO1Ej6T441YjQ1")
             ],[      
             InlineKeyboardButton("ℹ️ 𝙷𝙴𝙻𝙿 ℹ️", callback_data="help"),
             InlineKeyboardButton("💫 𝙰𝙱𝙾𝚄𝚃 💫", callback_data="about")
@@ -90,7 +90,7 @@ async def start(client, message):
             InlineKeyboardButton("➕️ 𝙰𝙳𝙳 𝙼𝙴 𝚃𝙾 𝚈𝙾𝚄𝚁 𝙶𝚁𝙾𝚄𝙿 ➕️", url=f"http://t.me/{temp.U_NAME}?startgroup=true")
             ],[
             InlineKeyboardButton("🔍 𝚂𝙴𝙰𝚁𝙲𝙷 🔍", switch_inline_query_current_chat=''), 
-            InlineKeyboardButton("💠 𝙲𝙷𝙰𝙽𝙽𝙴𝙻 💠", url="https://t.me/tamilhb")
+            InlineKeyboardButton("💠 𝙲𝙷𝙰𝙽𝙽𝙴𝙻 💠", url="https://t.me/+AaJO1Ej6T441YjQ1")
             ],[      
             InlineKeyboardButton("ℹ️ 𝙷𝙴𝙻𝙿 ℹ️", callback_data="help"),
             InlineKeyboardButton("💫 𝙰𝙱𝙾𝚄𝚃 💫", callback_data="about")
